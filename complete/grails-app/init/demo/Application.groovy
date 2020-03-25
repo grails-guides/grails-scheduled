3b48c4b7959ce1373ef1f8c921d530362b7689ab
niravassar <assarn@objectcomputing.com>
2020-03-25 14:20:28 -0500

    need spring annotation for scheduling

--- complete/grails-app/init/demo/Application.groovy
+++ complete/grails-app/init/demo/Application.groovy
@@ -4,9 +4,11 @@ import grails.boot.GrailsApp
 import grails.boot.config.GrailsAutoConfiguration
 import groovy.transform.CompileStatic
 import org.springframework.context.annotation.ComponentScan
+import org.springframework.scheduling.annotation.EnableScheduling
 
 @CompileStatic
 @ComponentScan('demo') //<1>
+@EnableScheduling //<2>
 class Application extends GrailsAutoConfiguration {
     static void main(String[] args) {
         GrailsApp.run(Application, args)
